b5bf1ada93a6d61c5ebd8c63ab0dab844d217dde
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-10-04 14:04:57 +0300

    📒 feat: adding cors and body parser

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,8 +3,13 @@ dotenv.config();
 import env from "./utils/validateEnv";
 import express, { Request, Response, Express } from "express";
 import connectToMongoDB from "./db/connection";
+
+import cors from "cors";
 const app: Express = express();
 const port: number | string = env.PORT || 8000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the Node.js Express Server!");
