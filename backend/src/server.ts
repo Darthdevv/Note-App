151813caf03dcceef0798f85c56a9c3fa2534566
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-10-05 13:43:45 +0300

    📒 feat: adding routes for notes and users

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,57 @@
 import dotenv from "dotenv";
 dotenv.config();
 import env from "./utils/validateEnv";
-import express, { Request, Response, Express } from "express";
+import express, { Request, Response, NextFunction, Express } from "express";
 import connectToMongoDB from "./db/connection";
-
 import cors from "cors";
+import userRouter from "./routes/user.routes";
+import noteRouter from "./routes/note.routes";
+
+// Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED EXCEPTION! 💥 shutting down...");
+  process.exit(1);
+});
+
 const app: Express = express();
 const port: number | string = env.PORT || 8000;
+
+// Middleware setup
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Middleware to add request time
+app.use((req: Request, res: Response, next: NextFunction) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// Define routes
+app.use("/api/notes", noteRouter);
+app.use("/api/users", userRouter);
+
+// Error handling
+app.use(notFound);
+app.use(globalErrorHandler);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the Node.js Express Server!");
 });
 
-app.listen(port, () => {
-  connectToMongoDB();
+// Connect to MongoDB
+connectToMongoDB();
+const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+});
+
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err: Error) => {
+  console.error(err.name, err.message);
+  console.error("UNHANDLED REJECTION! 💥 Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
 });
