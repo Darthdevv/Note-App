8a190b09f821246e03d1da41d7739f55b3c7bde5
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-10-10 10:46:21 +0300

    📒 feat: preparing to fetch notes data from backend

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,37 +2,20 @@
 
 const Card = () => {
   return (
-    <div className="flex flex-col w-[25%] bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
+    <div className="flex flex-col w-[25%] bg-[#F7F18A] border shadow-sm rounded-xl dark:bg-neutral-900 border-[#F2D161] dark:shadow-neutral-700/70">
       <div className="p-4 md:p-5">
         <h3 className="text-lg font-bold text-gray-800 dark:text-white">
           Card title
         </h3>
         <p className="mt-2 text-gray-500 dark:text-neutral-400">
-          With supporting text below as a natural lead-in to additional content.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio nihil
+          culpa blanditiis. Non reprehenderit iste aperiam saepe voluptates
+          accusamus culpa? A, iure voluptatem? Aut totam quia nobis. Ex, dolorum
+          dignissimos.
         </p>
-        <a
-          className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 decoration-2 hover:text-blue-700 hover:underline focus:underline focus:outline-none focus:text-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-600 dark:focus:text-blue-600"
-          href="#"
-        >
-          Card link
-          <svg
-            className="shrink-0 size-4"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="m9 18 6-6-6-6"></path>
-          </svg>
-        </a>
       </div>
-      <div className="bg-gray-100 border-t rounded-b-xl py-3 px-4 md:py-4 md:px-5 dark:bg-neutral-900 dark:border-neutral-700">
-        <p className="mt-1 text-sm text-gray-500 dark:text-neutral-500">
+      <div className="bg-[#F2D161] border-t rounded-b-xl py-3 px-4 md:py-4 md:px-5 dark:bg-neutral-900 border-[#F2D161]">
+        <p className="mt-1 text-sm dark:bg-neutral-900">
           Last updated 5 mins ago
         </p>
       </div>
