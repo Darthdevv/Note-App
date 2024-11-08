ebfa28f6e276d0bc395429e36a99548f7da23951
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-11-08 13:45:21 +0200

    📒 feat: creating user model

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,33 @@
+import mongoose, {InferSchemaType} from "mongoose";
+const {Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      select: false,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+type User = InferSchemaType<typeof userSchema>;
+
+export const User = model<User>("User", userSchema);
